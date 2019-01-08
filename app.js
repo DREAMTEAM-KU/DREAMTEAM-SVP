a4a98f023282e3cf8a862690cfa241005038dfbd
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2019-01-08 23:20:45 +0700

    add https connection

--- app.js
+++ app.js
@@ -14,3 +14,27 @@ app.use("/", routes);
 app.listen(PORT, () => {
   console.log("server is started on port " + PORT);
 });
+
+const privateKey = fs.readFileSync(
+  "/etc/letsencrypt/live/tesatopgun.thitgorn.com/privkey.pem",
+  "utf8"
+);
+const certificate = fs.readFileSync(
+  "/etc/letsencrypt/live/tesatopgun.thitgorn.com/cert.pem",
+  "utf8"
+);
+const ca = fs.readFileSync(
+  "/etc/letsencrypt/live/tesatopgun.thitgorn.com/chain.pem",
+  "utf8"
+);
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+};
+
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(8081, () => {
+  console.log("HTTPS Server running on port 8081");
+});
