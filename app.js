5793994db35f574c8ab737ff644bda7bdc2e6d7c
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2019-01-08 23:39:51 +0700

    add cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
+const cors = require('cors')
 const fs = require("fs");
 
 const routes = require("./routes");
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(bodyParser.json());
-
+app.use(cors())
 app.use("/", routes);
 
 app.listen(PORT, () => {
