830c1345226a1231a7482c5e9a4a2d110f21bf60
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2019-01-10 20:40:49 +0700

    fix migration

--- src/logic/ml.js
+++ src/logic/ml.js
@@ -1,4 +1,5 @@
 const { list } = require("../db/function/Ml");
+const { migration } = require("../db/function/Ml");
 const moment = require("moment");
 
 async function getSanam(hours = 0) {
@@ -18,6 +19,29 @@ async function getSanam(hours = 0) {
   return data;
 }
 
+async function migrate(data) {
+  // get key
+  const key = Object.keys(data)[0];
+
+  const value = data[key];
+
+  const ymd = key.split("-");
+
+  const results = [];
+  for (var i = 0; i < value.length; i++) {
+    const date = new Date(ymd[0], ymd[1], ymd[2], i, 0, 0, 0);
+    const modified = {
+      time: date,
+      value: value[i]
+    };
+    const migrationed = await migration(modified);
+    results.push(migrationed);
+  }
+
+  return results;
+}
+
 module.exports = {
-  getSanam
+  getSanam,
+  migrate
 };
