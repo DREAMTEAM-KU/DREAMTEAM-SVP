75bc6107aff5cce6e9d5286b6428e49961c655a2
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2019-01-11 02:30:31 +0700

    return error

--- src/routes/Ml.js
+++ src/routes/Ml.js
@@ -1,4 +1,5 @@
 const { main } = require("../logic/migrate/ml");
+const { list } = require("../db/function/Ml");
 const { getSanam } = require("../logic/ml");
 
 const express = require("express");
@@ -12,12 +13,17 @@ router.get("/migrate", async (req, res) => {
 
 router.get("/getSanam", async (req, res) => {
   const { hours } = req.query;
-  const data = await getSanam(parseInt(hours, 10));
-  const output = data.map(d => d.value);
-  const result = {
-    number_of_tourist: output
-  };
-  res.send(result);
+  const lists = await list();
+  if (lists.length >= hours) {
+    const data = await getSanam(parseInt(hours, 10));
+    const output = data.map(d => d.value);
+    const result = {
+      number_of_tourist: output
+    };
+    res.send(result);
+  } else {
+    res.send("Error");
+  }
 });
 
 router.get("/predict", async (req, res) => {
