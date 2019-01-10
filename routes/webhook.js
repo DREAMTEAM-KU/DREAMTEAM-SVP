169f638125a914cb2a4586fca7fea46a700f91d8
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2019-01-10 11:18:31 +0700

    log

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,30 +1,33 @@
-const request = require('request')
+const request = require("request");
 const express = require("express");
 
 const router = express.Router();
 
 const HEADERS = {
-  'Content-Type': 'application/json',
-  'Authorization': 'Bearer {g5Qni05t88rHy3B4yuQUvZS//LsbYu7Ava6R5bvsIRMLZ3BBHnT386209dHstws6+4bijs98Zb0u4XmWGO/FKghGdueH8lORwAFJm16ff0SUAsxSardFX105Voj3spNooUUsmDq2L0M2SJ8AJ/ByQQdB04t89/1O/w1cDnyilFU=}'
-}
+  "Content-Type": "application/json",
+  Authorization:
+    "Bearer {g5Qni05t88rHy3B4yuQUvZS//LsbYu7Ava6R5bvsIRMLZ3BBHnT386209dHstws6+4bijs98Zb0u4XmWGO/FKghGdueH8lORwAFJm16ff0SUAsxSardFX105Voj3spNooUUsmDq2L0M2SJ8AJ/ByQQdB04t89/1O/w1cDnyilFU=}"
+};
 
 router.get("/webhook", (req, res) => {
+  console.log("GET /webhook");
   res.send("/webhook line api");
 });
 
 router.post("/webhook", (req, res) => {
+  console.log("POST /webhook");
   // do some thing
   // reply block
-  let reply_token = ''
-  let msg = ''
-  reply_token = req.body.events[0].replyToken
-  if (req.body.events[0].type == 'beacon') {
-    msg = JSON.stringify(req.body.events[0])
+  let reply_token = "";
+  let msg = "";
+  reply_token = req.body.events[0].replyToken;
+  if (req.body.events[0].type == "beacon") {
+    msg = JSON.stringify(req.body.events[0]);
   } else {
-    msg = req.body.events[0].message.text
+    msg = req.body.events[0].message.text;
   }
-  reply(reply_token, msg)
-  res.send(msg)
+  reply(reply_token, msg);
+  res.send(msg);
   // console.log(msg)
   // res.send();
 });
@@ -32,59 +35,65 @@ router.post("/webhook", (req, res) => {
 function push(msg) {
   let body = JSON.stringify({
     // push body
-    to: 'U348d69552676fcfe93b75b5d187d0652',
-    messages: [{
-      type: 'text',
-      text: msg
-    }]
-  })
+    to: "U348d69552676fcfe93b75b5d187d0652",
+    messages: [
+      {
+        type: "text",
+        text: msg
+      }
+    ]
+  });
   // curl
-  curl('push', body)
+  curl("push", body);
 }
 
 function reply(reply_token, msg) {
-
   let body = JSON.stringify({
     replyToken: reply_token,
-    messages: [{
-      type: 'text',
-      text: msg
-    }]
-  })
+    messages: [
+      {
+        type: "text",
+        text: msg
+      }
+    ]
+  });
 
-if( msg === "Admin_Mon"){
-    body =  JSON.stringify({
-    replyToken: reply_token,
+  if (msg === "Admin_Mon") {
+    body = JSON.stringify({
+      replyToken: reply_token,
 
-    messages: [{
-        type: 'text',
-        text: 'Temp.'
-    },
-    {
-        type: 'text',
-        text: 'Humidity'
-    },
-    {
-      type: 'text',
-      text: 'in&out'
-  }]
-})
-
-}
-  curl('reply', body)
+      messages: [
+        {
+          type: "text",
+          text: "Temp."
+        },
+        {
+          type: "text",
+          text: "Humidity"
+        },
+        {
+          type: "text",
+          text: "in&out"
+        }
+      ]
+    });
+  }
+  curl("reply", body);
 }
 
 function curl(method, body) {
-  console.log('method:' + method)
-  request.post({
-    url: 'https://api.line.me/v2/bot/message/' + method,
-    headers: HEADERS,
-    body: body
-  }, (err, res, body) => {
-    console.log('status = ' + res.statusCode)
-    console.log(err);
-
-  })
+  console.log("method:" + method);
+  request.post(
+    {
+      url: "https://api.line.me/v2/bot/message/" + method,
+      headers: HEADERS,
+      body: body
+    },
+    (err, res, body) => {
+      console.log("status = " + res.statusCode);
+      console.log(err);
+    }
+  );
 }
 
 module.exports = router;
