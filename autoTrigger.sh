802f62b650520a6f00b4b5f5bc119c5262fd6b65
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2019-01-11 09:30:06 +0700

    Add timestamp log

--- autoTrigger.sh
+++ autoTrigger.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
+
+timestamp() {
+  date +"%T"
+}
+
 while [ true ]
 do
 
 echo "indexing"
+timestamp
 curl -X POST -H "Content-Type: application/json" -d '{}' http://localhost/indexDB
 sleep 3600
 
